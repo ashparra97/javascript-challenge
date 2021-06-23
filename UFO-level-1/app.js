e221004e7dfffd7036376dc56f1de0c9b3e535c2
ashparra97 <ashparra97@arizona.edu>
2021-06-22 20:56:00 -0700

    Update app.js

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -1,19 +1,43 @@
+// Test that console log is working
+console.log("hi there")
+
 // Get data from from data.js
 var tableData = data;
+console.log(tableData);
 
 /////////////////////
 // GOAL 1: Have data display on page
-// Create variable that will hold column names 
-var columns = ["Date", "City", "State", "Country", "Shape", "Duration", "Comments"];
 
-
-// Reference to HTML (d3.select)
 // Select table (to later append to)
 var tbody = d3.select("tbody");
 // Append a table to webpage using data.js
 
 // Add new rows of data for each UFO sighting
+tableData.forEach((witness) => {
+    // Test if pulling each witness report individually from data array
+    //console.log(witness);
 
+    // Append a row with each witness report information
+    var row = tbody.append("tr"); 
+    // Checked html and rows were added to table body
+
+    // Test if rows are added for each witness 
+    //console.log(row);
+
+    // Use object entries to console.log each UFO witness report 
+    Object.entries(witness).forEach(function([key, value]) {
+        console.log(key, value);
+
+        // Add a cell for each value now that we have rows and values 
+        var cell = row.append("td");
+        // Checked html and cells were added to table body
+
+        // Add content to cells
+        cell.text(value); 
+
+
+    });
+});
 
 
 /////////////////////
@@ -26,8 +50,10 @@ var button = d3.select("#filter-btn");
 // Create event listener and create a function to find value of what user entered 
 button.on("click", function() {
 
-    var input2 = d3.select("#datetime").property("value");
-    console.log(input2);
+    var input2 = d3.select("#datetime");
+    var inputvalue = input2.property("value");
+    console.log(inputvalue);
+    
 
 
 
